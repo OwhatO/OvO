2a79ec9a16f9be6e35d7d07bcccd503b8853cd1a
Fenz <uukoo@163.com>
2018-09-17 22:07:43 +0800

    Listener.js export several shortcut function like onClick(), to creating listeners

--- Listener.js
+++ Listener.js
@@ -31,3 +31,10 @@ export default class Listener
 		eventTarget.addEventListener( this[EVENT_NAME], this[CALLBACK], );
 	}
 }
+
+export const onClick= callback=> new Listener( 'click', callback, );
+export const onDbclick= callback=> new Listener( 'dbclick', callback, );
+export const onSubmit= callback=> new Listener( 'submit', callback, );
+export const onFocus= callback=> new Listener( 'focus', callback, );
+export const onBlur= callback=> new Listener( 'blur', callback, );
+export const onInput= callback=> new Listener( 'input', callback, );
